3ddd5bf0d0bd45b59c715520048bb086231ea289
yuchenglu <yuchengl@asu.edu>
2020-03-31 23:40:05 -0700

    forget_password(I just finish a part of this part)

--- login/urls.py
+++ login/urls.py
@@ -24,5 +24,9 @@ app_name = "login"
 urlpatterns = [
     path('', views.login_user, name='login_base'),
     path('block/', views.login_block, name='login_block'),
-    path('otp', views.verify_otp, name='verify_otp')
+    path('otp', views.verify_otp, name='verify_otp'),
+    #follow path use to forget jassword
+    path('forget_password/',auth_views.ForgetPasswordView.as_view(template_name='forget_passwordt.html'),name = 'forget_password'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name='password_reset.html.'),name = 'password_reset'),
+    path('reset_success/',auth_views.ResetSuccessView.as_view(template_name='reset_success.html.'),name = 'reset_success')
 ]
