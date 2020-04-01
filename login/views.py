3f3c3691dd056ad797682ef6aa68ca54f21f0f5a
yucheng888 <60374626+yucheng888@users.noreply.github.com>
2020-04-01 15:48:38 -0700

    Update views.py

--- login/views.py
+++ login/views.py
@@ -102,6 +102,13 @@ def verify_otp(request):
             if form.cleaned_data['otp'] == request.session['token']:
                 userObj = authenticate(username=request.session['username'], password=request.session['password'])
                 login(request,userObj)
+                
+                #create login history
+                d1 = timezone.now()
+                history_time = d1.strftime("%Y-%m-%d")
+                history = AllLogin(user = 'user_id', date = history_time )
+                history.save()
+                
                 request.session['last_activity'] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
                 return HttpResponseRedirect('http://127.0.0.1:8000/user_home')
         return HttpResponse("Login Failed!! Wrong OTP")
